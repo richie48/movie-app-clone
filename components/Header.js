399799f2967bb12169c47c1ba439cca27f40b2fc
richie48 <odiorrichard@gmail.com>
2021-11-07 22:53:01 +0000

    added all the header items

--- components/Header.js
+++ components/Header.js
@@ -4,9 +4,14 @@ import { BadgeCheckIcon,CollectionIcon,HomeIcon,LightningBoltIcon,SearchIcon,Use
 
 function Header() {
     return (
-        <header className=''>
-            <div>
-                <HeaderItem title='Home' Icon={HomeIcon}/>
+        <header className='flex flex-col sm:flex-row justify-between'>
+            <div className='flex flex-row flex-grow justify-evenly max-w-2xl'>
+                <HeaderItem title='HOME' Icon={HomeIcon}/>
+                <HeaderItem title='TREDING' Icon={LightningBoltIcon}/>
+                <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon}/>
+                <HeaderItem title='COLLECTIONS' Icon={CollectionIcon}/>
+                <HeaderItem title='SEARCH' Icon={SearchIcon}/>
+                <HeaderItem title='ACCOUNT' Icon={UserIcon}/>
             </div>
             <Image className="object-contain" src="https://links.papareact.com/ua6" width={200} height={100}/>
             
--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -1,11 +1,10 @@
 function HeaderItem({Icon,title}) {
     return (
-        <div className='group'>
-            <Icon className='h-8'/>
+        <div className='flex flex-col group cursor-pointer items-center w-12 sm:w-20 hover:text-white '>
+            <Icon className='h-8 mb-1 group-hover:animate-bounce mt-2'/>
             <p className='opacity-0 group-hover:opacity-100 tracking-widest'>{title}</p>
             
         </div>
     )
 }
-
 export default HeaderItem
