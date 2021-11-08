f843cfde840e83e730ca200bbed1e0758f14bc75
richie48 <odiorrichard@gmail.com>
2021-11-08 10:17:56 +0000

    some serverside rendering

--- components/Nav.js
+++ components/Nav.js
@@ -1,12 +1,17 @@
-import request from '../utils/request';
+import requests from '../utils/requests';
+import {useRouter} from 'next/router'
 
 function Nav() {
+    const router=useRouter()
+
+
     return (
-        <nav>
-            <div className='flex px-10 sm:px-20 whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll'>
-                {Object.entries(request).map(([key,{title,url}])=> <h2 className=" last:pr-20 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500" key={key}>{title}</h2> )}
+        <nav className='relative'>
+            <div className='flex px-10 sm:px-20 whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide'>
+                {Object.entries(requests).map(([key,{title,url}])=> <h2 onClick={()=> router.push(`/genre=${key}`)} className="text-2xl last:pr-20 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500" key={key}>{title}</h2> )}
 
             </div>
+            <div className='absolute top-0 right-0 w-1/12 h-8 bg-gradient-to-l from-[#06202A]'></div>
         </nav>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,11 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
+import Results from '../components/Results'
+import requests from '../utils/requests'
 
-export default function Home() {
+export default function Home({results}) {
+  console.log(props)
   return (
     <div >
       <Head>
@@ -14,6 +17,20 @@ export default function Home() {
 
       <Header/>
       <Nav/>
+      <Results results={results}/>
     </div>
   )
 }
+
+// What we want rendered on the server side
+export async function getServerSideProps(context){
+  const genre=context.query.genre;
+
+  const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`).then((res)=>res.json())
+  return{
+    props:{
+      results:request.results
+    }
+  }
+}
+
