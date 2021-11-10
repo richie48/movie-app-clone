13ecf119a63dd041973eb607dac00690b4e248b4
richie48 <odiorrichard@gmail.com>
2021-11-10 11:19:46 +0000

    cleaned up displays

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,8 +1,22 @@
-function Thumbnail({key,result}) {
-    console.log(result)
+import Image from 'next/image'
+import { ThumbUpIcon } from '@heroicons/react/outline'
+function Thumbnail({result}) {
+    const BASE_URL="https://image.tmdb.org/t/p/original"
     return (
-        <div>
-            <h1 className='text-white'>Thumbnail</h1>
+        <div className='p-2 group cursor-pointer'>
+            <Image
+            layout='responsive'
+            src={`${BASE_URL}${result.backdrop_path || result.poster_path}`|| `${BASE_URL}${result.poster_path}`   }
+            height={1080}
+            width={1920}/>
+            <div>
+            <p className='truncate max-w-md'>{result.overview}</p>
+            <h2>{result.title || result.original_name}</h2>
+            <p className='flex items-center opacity-0 group-hover:opacity-100'>
+                {result.media_type && `${result.media_type} .`}{' '}{result.release_date || result.first_air_date} .{' '} 
+                <ThumbUpIcon className='h-5 mx-2'/>{result.vote_count}</p>
+            
+            </div>
         </div>
     )
 }
